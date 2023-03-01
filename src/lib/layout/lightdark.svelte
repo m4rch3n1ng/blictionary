be85968f4f02733dcd038779cae1c90a4364b226
m4rch <m4rch3n1ng@gmail.com>
2023-03-01 17:34:40 +0100

    remove toggle on-load flicker on os-level darkmode

--- src/lib/layout/lightdark.svelte
+++ src/lib/layout/lightdark.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { onMount } from "svelte"
 	import { page } from "$app/stores"
     import { initTheme, invertTheme } from "$lib/theme"
 	import Moon from "$lib/assets/moon.svg.svelte"
@@ -7,16 +6,6 @@
 
 	export let init: string | undefined
 	const theme = initTheme(init)
-
-	onMount(() => {
-		const matchMedia = window.matchMedia("(prefers-color-scheme: dark)")
-		if (!init) theme.set(matchMedia.matches ? "dark" : "light", false)
-
-		window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", ( ev ) => {
-			const isCookie = theme.cookie()
-			if (!isCookie) theme.set(ev.matches ? "dark" : "light", false)
-		})
-	})
 
 	let btn: HTMLButtonElement
 	function submit () {
@@ -29,11 +18,12 @@
 <!-- todo auto theme (dropdown?) -->
 <form action="/app/theme" on:submit|preventDefault={submit} >
 	<button bind:this={btn} type="submit">
-		{#if $theme === "dark"}
+		<div class="dark {$theme ? $theme === "dark" : ""}">
 			<Moon />
-		{:else}
+		</div>
+		<div class="light {$theme ? $theme === "light" : ""}">
 			<Sun />
-		{/if}
+		</div>
 	</button>
 	<input type="text" name="theme" value={invertTheme(init)} />
 	<input type="text" name="url" value={$page.url.pathname} />
@@ -60,5 +50,31 @@
 		background-color: var(--svg-focus-background);
 		border-radius: 6px;
 		transition: background-color 0.2s ease;
+	}
+
+	.dark, .light {
+		display: inline-block;
+	}
+
+	.dark {
+		display: none;
+	}
+
+	@media screen and (prefers-color-scheme: dark) {
+		.dark {
+			display: inline-block;
+		}
+
+		.light {
+			display: none;
+		}
+	}
+
+	.light.false, .dark.false {
+		display: none
+	}
+
+	.light.true, .dark.true {
+		display: inline-block;
 	}
 </style>
