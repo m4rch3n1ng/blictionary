8af11bac668906b40ab7a6baac9bee789944b9ae
m4rch <m4rch3n1ng@gmail.com>
2023-03-05 02:26:56 +0100

    add notes to self

--- src/lib/fuzzy.ts
+++ src/lib/fuzzy.ts
@@ -1,7 +1,10 @@
 import type { smallEntry } from "./entry"
 
 // todo refine exclusion [^\s,.!$] probably
-// https://perldoc.perl.org/perlunicode#Unicode-Character-Properties
+// need to exclude: more punctuation, fucked up punctuation
+// ? https://perldoc.perl.org/perlunicode#Unicode-Character-Properties
+// also i have the feeling that this is not hard to break somehow
+// todo somewhere remove quotes, brackets?, parenthesis?
 const regexOfDoom = /^([^\s,.!$]+)[\s,.!$]*([^\s,!$]+)?[\s,.!$]*(\d+)?/
 
 // todo actually name stuff
@@ -21,12 +24,13 @@ export function fuzz ( allEntries: smallEntry[], search: string ): smallEntry[] 
 
 function initWordRegex ( word: string ) {
 	const all = [ ...word ].map(_escReg)
-	// todo maybe limit length?
+	// todo maybe limit [^\\s,.!$] amt
+	// todo extra score for positional / full word match
 	const $all = all.length === 1 ? [ all ] : all.map(( _v, i ) => replaceAtIndex(all, i, "[^\\s,.!$]*"))
 	const allReg = $all.map(( str ) => new RegExp(`${str.join("")}`, "i"))
 
 	return {
-		// todo better scoring
+		// todo better scoring ?
 		score ( word: string ) {
 			return allReg.reduce(( prev, curr ) => prev + ( curr.test(word) ? 1 : 0 ), 0)
 		}
@@ -38,7 +42,6 @@ function replaceAtIndex ( arr: string[], i: number, rep: string ) {
 	a.splice(i, 1, rep)
 	return a
 }
-
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#escaping 
 function _escReg ( w: string ) {
