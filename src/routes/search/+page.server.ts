c26e9be94895b2070a831071a78332c311f9ba94
m4rch <m4rch3n1ng@gmail.com>
2023-03-10 19:59:42 +0100

    slugify search redirect

--- src/routes/search/+page.server.ts
+++ src/routes/search/+page.server.ts
@@ -1,6 +1,7 @@
 import { redirect } from "@sveltejs/kit"
 import { filter as fuzzyFilter } from "fuzzyjs"
 import type { PageServerLoadEvent } from "./$types"
+import { slugify } from "$lib/markdown"
 
 export async function load ({ parent, url }: PageServerLoadEvent ) {
 	const { allMeta } = await parent()
@@ -15,7 +16,8 @@ export async function load ({ parent, url }: PageServerLoadEvent ) {
 
 	const allFind = allMeta.filter(({ word }) => word === query)
 	if (allFind.length === 1 && allFind[0]) {
-		throw redirect(301, `/view/${allFind[0].id}/${allFind[0].word}`) // todo
+		const entry = allFind[0]
+		throw redirect(301, `/view/${entry.id}/${slugify(entry.word)}`) // todo
 	}
 
 	const filteredMeta = allMeta.filter(fuzzyFilter(query, { iterator: ({ word }) => word }))
