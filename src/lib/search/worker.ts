752704a6ddb80a068a61c6e0078ede819052914f
m4rch <m4rch3n1ng@gmail.com>
2023-03-14 01:37:47 +0100

    debounce fuzzy off main thread in webworker

---
+++ src/lib/search/worker.ts
@@ -0,0 +1,20 @@
+import type { smallEntry } from "$lib/entry"
+import { expose } from "comlink"
+import { fuzzy } from "./fuzzy"
+
+let allEntries: smallEntry[]
+function init ( newEntries: smallEntry[] ) {
+	allEntries = newEntries
+}
+
+function search ( value: string ) {
+	const sorted = fuzzy(value, allEntries)
+	return sorted.slice(0, 10)
+}
+
+export interface worker {
+	init: typeof init,
+	search: typeof search
+}
+
+expose(<worker>{ init, search })
