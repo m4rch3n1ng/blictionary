1a481ce750b087501db10975ac182905f6583224
m4rch <m4rch3n1ng@gmail.com>
2023-03-17 18:47:43 +0100

    sveltekit-preload, code quality, consistency

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,7 @@
+import type { MaybePromise } from "$app/forms"
 import type { Handle } from "@sveltejs/kit"
 
-export const handle: Handle = ({ event, resolve }) => {
+export function handle ({ event, resolve }: Parameters<Handle>[0] ): MaybePromise<Response> {
 	const theme = event.cookies.get("theme")
 	return resolve(event, {
 		transformPageChunk: ({ html }) => html.replace("%theme%", theme || "")
