2e34b468b4e8c801baa6fae782a624557d07baf1
m4rch <m4rch3n1ng@gmail.com>
2023-03-17 20:18:40 +0100

    init file cache

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,6 +2,11 @@ import { fetchAllEntries } from "$lib/entry"
 import type { LayoutServerLoadEvent } from "./$types"
 
 export async function load ({ cookies }: LayoutServerLoadEvent ) {
+	// load times are only slow because of data serialization
+	// todo find some way to compress and decompress on the server side
 	const allEntries = await fetchAllEntries()
-	return { allEntries, theme: cookies.get("theme") }
+	return {
+		allEntries,
+		theme: cookies.get("theme")
+	}
 }
