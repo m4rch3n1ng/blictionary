6430b7051d6b24367c027d6182559faf44c89364
may <m4rch3n1ng@gmail.com>
2023-03-21 17:53:12 +0100

    replace fetchAllMeta with export cache

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,9 +1,9 @@
-import { fetchAllEntries } from "$lib/entry"
+import { cache } from "$lib/entry"
 
 export async function load ({ cookies }) {
 	// load times are only slow because of data serialization
 	// todo find some way to compress and decompress on the server side
-	const allEntries = await fetchAllEntries()
+	const allEntries = await cache.get()
 	return {
 		allEntries,
 		theme: cookies.get("theme")
