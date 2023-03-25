de7575aab60dae20ecdd14d09e29581e55a3629c
may <m4rch3n1ng@gmail.com>
2023-03-25 03:25:29 +0100

    init entryFilter

--- src/routes/view/[id=integer]/[name]/+page.server.ts
+++ src/routes/view/[id=integer]/[name]/+page.server.ts
@@ -1,11 +1,9 @@
-import { cache, getEntry, hasEntry } from "$lib/entry"
+import { cache, getEntry, hasEntry, type Entry, type smallEntry } from "$lib/entry"
 import { slugify } from "$lib/markdown"
 import { error, redirect } from "@sveltejs/kit"
 import type { PageServerLoadEvent } from "./$types"
 
 export async function load ({ params }: PageServerLoadEvent ) {
-	const allEntries = await cache.get()
-
 	const id = params.id
 	if (!hasEntry(id)) throw error(404, "not found")
 
@@ -13,9 +11,51 @@ export async function load ({ params }: PageServerLoadEvent ) {
 	const entrySlug = slugify(entry.word)
 	if (entrySlug !== params.name) throw redirect(301, `/view/${params.id}/${entrySlug}`)
 
+	const allEntries = await cache.get()
+	const filteredEntries = filterEntries(allEntries, entry)
+
 	return {
-		// filter allEntries for useful ones only
-		allEntries: allEntries.slice(0, 1),
+		allEntries: filteredEntries,
 		entry
+	}
+}
+
+// look for references in
+// + definitions
+// 	.text
+// 	.quotes
+// 	.sub.text
+// 	.sub.quotes
+// + sub
+// 	.definition
+// 	.quotes
+// 
+// $ quotes
+// 	.quotes.note
+// 	.quotes.text ?
+const matcher = /\[\[([^\n]+?), ([^\n]+?\.)\]\]/g
+function filterEntries ( allEntries: smallEntry[], entry: Entry ) {
+	const allToFind: RegExpMatchArray[] = []
+
+	allToFind.push(...matchOne(entry.etymology))
+	if (entry.forms) allToFind.push(...matchOne(entry.forms))
+	if (entry.pronounciation.note) allToFind.push(...matchOne(entry.pronounciation.note))
+
+	const filteredEntries = allToFind.map(findOne.bind(null, allEntries)).filter(( entry ) => entry !== null)
+	return filteredEntries
+}
+
+function findOne ( allEntries: smallEntry[], [ , word, wordClass ]: RegExpMatchArray ) {
+	const found = allEntries.find(( entry ) => (
+		entry.word === word && ( Array.isArray(entry.class) ? entry.class.includes(wordClass!) : entry.class === wordClass )
+	))
+	return found || null
+}
+
+function matchOne ( inp: string | string[] ) {
+	if (Array.isArray(inp)) {
+		return inp.join("\n").matchAll(matcher)
+	} else {
+		return inp.matchAll(matcher)
 	}
 }
