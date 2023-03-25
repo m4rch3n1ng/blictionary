8fcd930da50b5293d3bfd19d0944f73e46ebe60f
may <m4rch3n1ng@gmail.com>
2023-03-25 03:50:12 +0100

    finish basic entry filter

--- src/routes/view/[id=integer]/[name]/+page.server.ts
+++ src/routes/view/[id=integer]/[name]/+page.server.ts
@@ -1,4 +1,4 @@
-import { cache, getEntry, hasEntry, type Entry, type smallEntry } from "$lib/entry"
+import { cache, getEntry, hasEntry, type Entry, type Quote, type smallEntry, type SubEntry } from "$lib/entry"
 import { slugify } from "$lib/markdown"
 import { error, redirect } from "@sveltejs/kit"
 import type { PageServerLoadEvent } from "./$types"
@@ -20,26 +20,17 @@ export async function load ({ params }: PageServerLoadEvent ) {
 	}
 }
 
-// look for references in
-// + definitions
-// 	.text
-// 	.quotes
-// 	.sub.text
-// 	.sub.quotes
-// + sub
-// 	.definition
-// 	.quotes
-// 
-// $ quotes
-// 	.quotes.note
-// 	.quotes.text ?
+// this should probably be added to the cache
 const matcher = /\[\[([^\n]+?), ([^\n]+?\.)\]\]/g
 function filterEntries ( allEntries: smallEntry[], entry: Entry ) {
 	const allToFind: RegExpMatchArray[] = []
 
-	allToFind.push(...matchOne(entry.etymology))
-	if (entry.forms) allToFind.push(...matchOne(entry.forms))
+	allToFind.push(...matchTwo(entry.etymology))
+	if (entry.forms) allToFind.push(...matchTwo(entry.forms))
 	if (entry.pronounciation.note) allToFind.push(...matchOne(entry.pronounciation.note))
+
+	allToFind.push(...fromDef(entry.definitions))
+	if (entry.sub) allToFind.push(...fromSub(entry.sub))
 
 	const filteredEntries = allToFind.map(findOne.bind(null, allEntries)).filter(( entry ) => entry !== null)
 	return filteredEntries
@@ -52,10 +43,37 @@ function findOne ( allEntries: smallEntry[], [ , word, wordClass ]: RegExpMatchA
 	return found || null
 }
 
-function matchOne ( inp: string | string[] ) {
+function matchTwo ( inp: string[] | string ) {
 	if (Array.isArray(inp)) {
-		return inp.join("\n").matchAll(matcher)
+		return matchOne(inp.join("\n"))
 	} else {
-		return inp.matchAll(matcher)
+		return matchOne(inp)
 	}
+}
+
+function matchOne ( inp: string ) {
+	return inp.matchAll(matcher)
+}
+
+function fromDef ( defs: Entry["definitions"] ): RegExpMatchArray[] {
+	return defs.flatMap(( def ) => {
+		if ("sub" in def) {
+			const q = fromQuotes(def.quotes)
+			const t = def.text ? matchOne(def.text) : []
+			return [ ...t, ...q, ...fromDef(def.sub) ]
+		} else {
+			const q = fromQuotes(def.quotes)
+			return [ ...matchOne(def.text), ...q ]
+		}
+	})
+}
+
+// .quotes.text ?
+function fromQuotes ( quotes: undefined | Quote[] ): RegExpMatchArray[] {
+	if (!quotes || quotes.length === 0) return []
+	return quotes.flatMap(( quote ) => quote.note ? [ ...quote.note.matchAll(matcher) ] : [])
+}
+
+function fromSub ( subDef: SubEntry[] ) {
+	return subDef.flatMap(( sub ) => [ ...matchOne(sub.definition), ...fromQuotes(sub.quotes) ])
 }
