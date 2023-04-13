02731e9152eb5248e30f0c6d6ad7cff1322ecb01
may <m4rch3n1ng@gmail.com>
2023-04-13 16:34:36 +0200

    init dropzone styling

--- src/routes/add/+page.svelte
+++ src/routes/add/+page.svelte
@@ -4,7 +4,32 @@
 
 
 <form enctype="multipart/form-data" action="/app/add" method="post">
-	<input name="file" type="file" />
+	<div class="dropzone">
+		<input name="file" type="file" />
+	</div>
 	<button type="submit">submit</button>
 	<input type="text" name="url" value={$page.url.href} hidden />
 </form>
+
+
+<style>
+	.dropzone {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding: 20px;
+		border-width: 2px;
+		border-radius: 2px;
+		border-color: #eeeeee;
+		border-style: dashed;
+		background-color: #fafafa;
+		color: #bdbdbd;
+		outline: none;
+		transition: border 0.24s ease-in-out;
+	}
+
+	.dropzone:focus {
+		border-color: #2196f3;
+	}
+</style>
